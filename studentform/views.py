5ae24c8cb5f24a7ed3ba5d8dc9d7323d496e33b2
Brian Magaña <brian.magana2929@alumnos.udg>
2023-09-28 15:15:09 -0600

    Student Form now uses a Factory

--- studentform/views.py
+++ studentform/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.db import transaction
+
 from .models import Student, Contact, School_Cycle
 from .forms import StudentForm
+from .factories import StudentFactory
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -10,21 +14,33 @@ def index(request):
     cycles = School_Cycle.objects.all()
     return render(request, "studentform/index.html", {'students':students, 'contacts':contacts, 'cycles':cycles})
 
-#Checar esta funcion, ya que el formulario no crea un nuevo estudiante.
 def createFormStudent(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
+            # Saves Form in the DataBase
+            '''
             code = form.cleaned_data['code']
             name = form.cleaned_data['name']
             status = form.cleaned_data['status']
 
-            # Save
             student = Student(code=code, name=name, status=status)
             student.save()
+            '''
+
+            # Factory
+            callStudentFactory()
 
             return redirect('index') # Put the name of the views in here to redirect.
     else:
         form = StudentForm()
     
     return render(request, "studentform/student_form.html", {'form': form})
+
+@transaction.atomic
+def callStudentFactory():
+    # Factory
+    students = StudentFactory.create_batch(13)
+
+    for student in students:
+        student.save()
