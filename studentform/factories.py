ea9f0d80ce4e9b47f02579e74bb760faa527e668
Brian Magaña <brian.magana2929@alumnos.udg>
2023-09-28 15:12:12 -0600

    Fix StudentFactory

--- studentform/factories.py
+++ studentform/factories.py
@@ -5,6 +5,6 @@ class StudentFactory(factory.Factory):
     class Meta:
         model = Student
     
-    code = factory.faker('random_int', min=210000000, max=300000000)
-    name = factory.faker('name')
+    code = factory.Faker('random_int', min=210000000, max=300000000)
+    name = factory.Faker('name')
     status = factory.Faker('random_element', elements=['activo', 'inactivo', 'art 34', 'art 35'])
