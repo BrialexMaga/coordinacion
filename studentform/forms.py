b9d09e88376c05eab2d4d38090920f42f0c988d3
Brian Magaña <brian.magana2929@alumnos.udg>
2023-10-02 13:23:01 -0600

    Student Form now creates forms from a model

--- studentform/forms.py
+++ studentform/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from .models import Student, Contact
 
-class StudentForm(forms.Form):
-    code = forms.CharField(max_length=9, label='Código')
-    name = forms.CharField(max_length=100, label='Nombre')
-    status = forms.CharField(max_length=10, label='Estatus')
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["code", "name", "status"]
     
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
