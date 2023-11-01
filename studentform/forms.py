6f294413a633cfb4fbbe02391dca41ef8a90fedf
Brian <brian.magana2929@alumnos.udg.mx>
2023-11-01 15:23:06 +0100

    Index and form now shows the new fields

--- studentform/forms.py
+++ studentform/forms.py
@@ -5,8 +5,24 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ["code", "name", "status", "admission_cycle"]
+        labels = {
+            "code": "Código",
+            "name": "Nombre",
+            "status": "Estatus",
+            "admission_cycle": "Ciclo de Admisión"
+        }
     
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ["phone", "email", "udg_email", "emergency_phone", "url_socialnet"]
+        fields = ["phone", "email", "udg_email", "emergency_phone", "url_socialnet",
+                  "company", "position"]
+        labels = {
+            "phone": "Teléfono",
+            "email": "Correo Electronico", 
+            "udg_email": "Correo de UDG", 
+            "emergency_phone": "Teléfono de Emergencia", 
+            "url_socialnet": "URL de Red Social",
+            "company": "Compañia donde trabaja", 
+            "position": "Posición"
+        }
