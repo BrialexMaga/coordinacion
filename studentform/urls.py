86093b5b33dc06cb20d1e7d65a5ddfdec44a83cd
Brian <brian.magana2929@alumnos.udg.mx>
2023-11-07 13:53:14 +0100

    Export specific student contact

--- studentform/urls.py
+++ studentform/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('export-contacts/', views.exportContacts, name='export-contacts'),
+    path('contactform/<int:idStudent>/export/', views.exportStudentData, name='export-student-contact'),
+
     path("createform/", views.createFormStudent, name="createFormStudent"),
     path("contactform/<int:idStudent>/", views.createFormContact, name='contact-form'),
     path("contact/<int:idStudent>/", views.showContact, name='show-contact'),
--- studentform/views.py
+++ studentform/views.py
@@ -30,6 +30,7 @@ def exportContacts(request):
     response['Content-Disposition'] = 'attachment; filename="contacts.csv"'
 
     writer = csv.writer(response)
+    # Headers
     writer.writerow(['Name', 
                      'Given Name', 'Additional Name', 'Family Name', 
                      'Yomi Name', 'Given Name Yomi', 'Additional Name Yomi', 'Family Name Yomi', 'Name Prefix', 
@@ -45,6 +46,7 @@ def exportContacts(request):
     contacts = Contact.objects.all()
 
     for contact in contacts:
+        # Data
         writer.writerow([f'{contact.idStudent.name} {contact.idStudent.first_last_name} {contact.idStudent.second_last_name}', 
                          contact.idStudent.name, "", f'{contact.idStudent.first_last_name} {contact.idStudent.second_last_name}', 
                          "", "", "", "", "",
@@ -57,6 +59,41 @@ def exportContacts(request):
                          "", "", "Red Social", contact.url_socialnet, "Codigo Escolar", 
                          contact.idStudent.code, "Estatus", contact.idStudent.status.status])
 
+    return response
+
+def exportStudentData(request, idStudent):
+    student = Student.objects.get(pk=idStudent)
+    contact = Contact.objects.get(idStudent=student)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{student.name}_{student.first_last_name}_{student.second_last_name}_contact.csv"'
+
+    writer = csv.writer(response)
+    # Headers
+    writer.writerow(['Name', 
+                     'Given Name', 'Additional Name', 'Family Name', 
+                     'Yomi Name', 'Given Name Yomi', 'Additional Name Yomi', 'Family Name Yomi', 'Name Prefix', 
+                     'Name Suffix', 'Initials', 'Nickname', 'Short Name', 'Maiden Name', 
+                     'Birthday', 'Gender Location', 'Billing Information Directory Server',	'Mileage Occupation', 'Hobby', 
+                     'Sensitivity',	'Priority', 'Subject',	'Notes', 'Language', 'Photo',
+                     'Group Membership', 'E-mail 1 - Type', 'E-mail 1 - Value', 'E-mail 2 - Type', 'E-mail 2 - Value',
+                     'Phone 1 - Type', 'Phone 1 - Value', 'Phone 2 - Type', 'Phone 2 - Value', 'Organization 1 - Type', 
+                     'Organization 1 - Name', 'Organization 1 - Yomi Name', 'Organization 1 - Title', 'Organization 1 - Department', 'Organization 1 - Symbol',
+                     'Organization 1 - Location', 'Organization 1 - Job Description', 'Website 1 - Type', 'Website 1 - Value', 'Custom Field 1 - Type', 
+                     'Custom Field 1 - Value', 'Custom Field 2 - Type', 'Custom Field 2 - Value'])
+    
+    # Data
+    writer.writerow([f'{contact.idStudent.name} {contact.idStudent.first_last_name} {contact.idStudent.second_last_name}', 
+                    contact.idStudent.name, "", f'{contact.idStudent.first_last_name} {contact.idStudent.second_last_name}', 
+                    "", "", "", "", "",
+                    "", "", "", "", "",
+                    "", "", "", "", "",
+                    "", "", "", "", "", "",
+                    "", "Personal", contact.email, "UDG", contact.udg_email, 
+                    "Emergencia", contact.emergency_phone, "Personal", contact.phone,  "", 
+                    contact.company, "", contact.position, "", "",
+                    "", "", "Red Social", contact.url_socialnet, "Codigo Escolar", 
+                    contact.idStudent.code, "Estatus", contact.idStudent.status.status])
     return response
 
 def showContact(request, idStudent):
