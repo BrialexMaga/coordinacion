58b7b0754685be161a8ed40d27968d935a5a3324
Brian <brian.magana2929@alumnos.udg.mx>
2023-11-21 15:40:28 +0100

    Make Model for studenthistory | Migrate to DB

--- studenthistory/admin.py
+++ studenthistory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subject, Section, GradePeriod, Course, CareerSubject
 
-# Register your models here.
+admin.site.register(Subject)
+admin.site.register(Section)
+admin.site.register(GradePeriod)
+admin.site.register(Course)
+admin.site.register(CareerSubject)
--- studenthistory/models.py
+++ studenthistory/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from studentform.models import School_Cycle, Student, Career
 
-# Create your models here.
+class Subject(models.Model):
+    idSubject = models.AutoField(primary_key=True)
+    key_subject = models.CharField(max_length=7)
+    name = models.CharField(max_length=50)
+    credits = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Section(models.Model):
+    idSection = models.AutoField(primary_key=True)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+    nrc = models.CharField(max_length=15)
+    section = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.section}"
+
+class GradePeriod(models.Model):
+    idGradePeriod = models.AutoField(primary_key=True)
+    code_name = models.CharField(max_length=3)
+    grade_period = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.grade_period} ({self.code_name})"
+
+class Course(models.Model):
+    idCourse = models.AutoField(primary_key=True)
+    school_cycle = models.ForeignKey(School_Cycle, on_delete=models.PROTECT)
+    student = models.ForeignKey(Student, on_delete=models.PROTECT)
+    section = models.ForeignKey(Section, on_delete=models.PROTECT)
+    grade = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)])
+    grade_period = models.ForeignKey(GradePeriod, on_delete=models.PROTECT)
+    upload_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.school_cycle} - {self.section}"
+
+class CareerSubject(models.Model):
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('career', 'subject')
