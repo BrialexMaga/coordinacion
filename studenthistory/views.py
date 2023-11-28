9dfccbec1b35ff878584b1bad438880a8263d846
Brian <brian.magana2929@alumnos.udg.mx>
2023-11-28 16:23:15 +0100

    Determine a subject in risk - Secuence of failed subjects

--- studenthistory/views.py
+++ studenthistory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import StudentCodeForm
 from studentform.models import Student
-from .models import Course, School_Cycle
+from .models import Course, School_Cycle, Subject
 from collections import OrderedDict
 import pickle
 import numpy as np
@@ -15,30 +15,76 @@ def searchPage(request):
                 student = Student.objects.get(code=student_code)
                 courses = Course.objects.filter(student=student).order_by('-school_cycle__year', 'school_cycle__cycle_period', 'section__subject__key_subject', 'grade_period')
 
-                cycles_list = []
-                cycles_index_list = courses.values_list('school_cycle__idCycle', flat=True).distinct()
-                for cycleId in cycles_index_list:
-                    cycle = School_Cycle.objects.get(idCycle=cycleId)
-                    cycles_list.append(cycle)
-
-                cycles_list = list(OrderedDict.fromkeys(cycles_list))
-
+                cycles_list = extractCycles(courses)
+                risk_subjects, len_risk_subjects = extractRiskSubjects(courses, cycles_list, student)
+                
                 # Medidas de listas
                 #len_list = len(courses)
                 #len_cycles = len(cycles_list)
 
+                # Call IA Model result
                 prediction = predictRisk(student)
 
-                return render(request, 'studenthistory/show_history.html', {'student': student, 'prediction': prediction, 
-                                                                            'courses': courses, 'cycles_list': cycles_list#,
-                                                                            #'len_list': len_list, 'len_cycles': len_cycles
-                                                                            })
+                return render(request, 'studenthistory/show_history.html', 
+                              {'student': student, 'prediction': prediction, 'courses': courses, 'cycles_list': cycles_list,
+                               'risk_subjects': risk_subjects, #'len_list': len_list, 'len_cycles': len_cycles,
+                               'len_risk_subjects': len_risk_subjects})
             except Student.DoesNotExist:
                 form.add_error('student_code', 'No se encontró ningún estudiante con este código.')
     else:
         form = StudentCodeForm()
 
     return render(request, 'studenthistory/search_page.html', {'form': form})
+
+def extractCycles(courses):
+    cycles = []
+    cycles_index_list = courses.values_list('school_cycle__idCycle', flat=True).distinct()
+    for cycleId in cycles_index_list:
+        cycle = School_Cycle.objects.get(idCycle=cycleId)
+        cycles.append(cycle)
+
+    cycles = list(OrderedDict.fromkeys(cycles))
+    return cycles
+
+def extractRiskSubjects(courses, cycles, student):
+    subject_indexes = courses.values_list('section__subject__idSubject', flat=True).distinct()
+    subjects_indexes = list(set(subject_indexes))
+
+    risk_courses = []
+    for index in subjects_indexes:
+        subject_registers = Course.objects.filter(student=student, 
+                                                  section__subject__idSubject = index).order_by('-school_cycle__year', 
+                                                                                                'school_cycle__cycle_period', 
+                                                                                                'grade_period')
+        subject_info = Subject.objects.get(idSubject=index)
+        if subject_info.has_extraordinary:
+            ordinary = subject_registers.filter(grade_period__code_name = 'OE')
+            extraordinary = subject_registers.filter(grade_period__code_name = 'E')
+
+            if len(ordinary) > 1:
+                if len(ordinary) == len(extraordinary):
+                    last_extraordinary = extraordinary.last()
+                    if last_extraordinary.grade < 60:
+                        risk_courses.append(subject_info)
+                else:
+                    last_ordinary = ordinary.last()
+                    if len(extraordinary) > 0:
+                        last_extraordinary = extraordinary.last()
+                        if last_ordinary.school_cycle == last_extraordinary.school_cycle:
+                            if last_extraordinary.grade < 60:
+                                risk_courses.append(subject_info)
+                        else:
+                            if last_ordinary.grade < 60:
+                                risk_courses.append(subject_info)
+                    else:
+                        if last_ordinary.grade < 60:
+                            risk_courses.append(subject_info)
+        else:
+            last_register = subject_registers.last()
+            if len(subject_registers) > 1 and last_register.grade < 60:
+                risk_courses.append(subject_info)
+    
+    return risk_courses, len(risk_courses)
 
 def predictRisk(student):
     model_path = 'IAmodels/IAmodel.pkl'
