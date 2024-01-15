28be81949066fd562f55a9e2dcc0ab5f32d58309
Brian <brian.magana2929@alumnos.udg.mx>
2024-01-15 16:59:58 +0100

    Add custom Login/Logout

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import MyLoginView, my_logout_view
 from . import views
 
 urlpatterns = [
     path('', views.landingPage, name='landing-page'),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('logout/', my_logout_view, name='logout'),
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 
 def landingPage(request):
     return render(request, 'landingpage/landing_page.html')
+
+class MyLoginView(LoginView):
+    template_name = 'landingpage/login.html'
+    redirect_authenticated_user = True
+
+class MyLogoutView(LogoutView):
+    next_page = '/login/'
+
+my_logout_view = MyLogoutView.as_view()
