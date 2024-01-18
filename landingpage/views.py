ee7947c258c4e07a433825294ad5ad566d90bd87
Brian <brian.magana2929@alumnos.udg.mx>
2024-01-18 13:23:06 +0100

    Add error message for wrong credentials

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib import messages
 from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
@@ -16,6 +17,10 @@ def landingPage(request):
     return render(request, 'landingpage/landing_page.html')
 
 class MyLoginView(LoginView):
+    def form_invalid(self, form):
+        messages.error(self.request, 'Credenciales incorrectas. Inténtalo de nuevo.')
+        return super().form_invalid(form)
+    
     template_name = 'landingpage/login.html'
     redirect_authenticated_user = True
 
