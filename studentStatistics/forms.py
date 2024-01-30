4651c927305a69032288bf9317213dd84bd6cc42
Brian <brian.magana2929@alumnos.udg.mx>
2024-01-30 16:55:02 +0100

    Add Empty Label

--- studentStatistics/forms.py
+++ studentStatistics/forms.py
@@ -3,4 +3,7 @@ from studentform.models import School_Cycle
 
 class FilterForm(forms.Form):
     ordered_cycles = School_Cycle.objects.all().order_by('-year', '-cycle_period')
-    idCycle = forms.ModelChoiceField(queryset=ordered_cycles, label='Generación')
+    idCycle = forms.ModelChoiceField(
+        queryset=ordered_cycles, 
+        label='Generación', 
+        empty_label="----Seleccione una Generación----")
