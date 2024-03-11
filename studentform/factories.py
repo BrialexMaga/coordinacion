272b500daa1a30e60845c5baf0ace4547b927235
Brian Magana <perenegro@gmail.com>
2024-03-11 15:32:39 -0600

    Upgrade StudentFactory and ContactFactory

--- studentform/factories.py
+++ studentform/factories.py
@@ -1,6 +1,6 @@
 import factory
 import random
-from .models import Student, Contact
+from .models import Student, Contact, Status, School_Cycle, Career, Syllabus
 
 class StudentFactory(factory.Factory):
     class Meta:
@@ -8,20 +8,14 @@ class StudentFactory(factory.Factory):
     
     code = factory.Faker('random_int', min=210000000, max=300000000)
     name = factory.Faker('name')
-    status = factory.Faker('random_element', elements=['activo', 'inactivo', 'art 34', 'art 35'])
-    
-    @classmethod
-    def _create(cls, model_class, *args, **kwargs):
-        min_num = 2010
-        max_num = 2023
-        random_num = random.randint(min_num, max_num)
-        elements = ['A', 'B']
-        random_element = random.choice(elements)
-        admission_cycle = f'{random_num}{random_element}'
-
-        kwargs['admission_cycle'] = admission_cycle
-
-        return super(StudentFactory, cls)._create(model_class, *args, **kwargs)
+    first_last_name = factory.Faker('last_name')
+    second_last_name = factory.Faker('last_name')
+    status = factory.LazyAttribute(lambda _: Status.objects.get(code_name='AC'))
+    exchange = factory.Faker('boolean', chance_of_getting_true=0)
+    admission_cycle = factory.LazyAttribute(lambda _: School_Cycle.objects.get(year=2023, cycle_period='B'))
+    last_cycle = factory.LazyAttribute(lambda _: School_Cycle.objects.get(year=2023, cycle_period='B'))
+    idCareer = factory.LazyAttribute(lambda _: Career.objects.get(code_name='INCO'))
+    syllabus = factory.LazyAttribute(lambda _: Syllabus.objects.get(name='2016'))
     
 class ContactFactory(factory.Factory):
     class Meta:
@@ -31,3 +25,4 @@ class ContactFactory(factory.Factory):
     email = factory.Faker('email')
     udg_email = factory.Faker('email')
     emergency_phone = factory.Faker('phone_number')
+    url_socialnet = factory.Faker('url')
