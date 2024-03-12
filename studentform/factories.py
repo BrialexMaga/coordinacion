64df7e57ed9e773b04d42d26c5fecdd5cef2255a
Brian Magana <perenegro@gmail.com>
2024-03-11 22:45:07 -0600

    Create CourseFactory and linked to new students creation

--- studentform/factories.py
+++ studentform/factories.py
@@ -1,6 +1,20 @@
 import factory
+from factory import post_generation
 import random
 from .models import Student, Contact, Status, School_Cycle, Career, Syllabus
+from studenthistory.models import Section, GradePeriod, Course
+
+#from .factories import CourseFactory
+
+class CourseFactory(factory.Factory):
+    class Meta:
+        model = Course
+
+    school_cycle = factory.LazyAttribute(lambda _: School_Cycle.objects.get(year=2023, cycle_period='B'))
+    grade = factory.Faker('random_int', min=50, max=100)
+    grade_period = factory.LazyAttribute(lambda _: GradePeriod.objects.get(code_name='OE'))
+    upload_date = factory.Faker('date_this_year')
+
 
 class StudentFactory(factory.Factory):
     class Meta:
@@ -16,7 +30,8 @@ class StudentFactory(factory.Factory):
     last_cycle = factory.LazyAttribute(lambda _: School_Cycle.objects.get(year=2023, cycle_period='B'))
     idCareer = factory.LazyAttribute(lambda _: Career.objects.get(code_name='INCO'))
     syllabus = factory.LazyAttribute(lambda _: Syllabus.objects.get(name='2016'))
-    
+
+
 class ContactFactory(factory.Factory):
     class Meta:
         model = Contact
